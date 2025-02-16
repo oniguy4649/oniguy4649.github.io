b4dffd9ba5cfa777681acfc67aa189842642f5b8
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2025-02-16 22:26:35 +0200

    Configure Astro site settings and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://oniguy4649.github.io',
+    base: '/notes',
+});
