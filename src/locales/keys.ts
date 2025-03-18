d6743b2e1f4e3ca5aafe29ed391a3cdbf5fb3f8f
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2025-03-19 00:21:27 +0200

    Add internationalization support with English, Japanese, and Chinese translations

---
+++ src/locales/keys.ts
@@ -0,0 +1,18 @@
+enum I18nKeys {
+  copy_right_author = "copy_right_author",
+  copy_right_publish_date = "copy_right_publish_date",
+  copy_right_license = "copy_right_license",
+
+  nav_bar_home = "nav_bar_home",
+  nav_bar_about = "nav_bar_about",
+  nav_bar_search_placeholder = "nav_bar_search_placeholder",
+
+  side_bar_categories = "side_bar_categories",
+  side_bar_tags = "side_bar_tags",
+  side_bar_view_more = "side_bar_view_more",
+
+  pages_tags_title = "pages_tags_title",
+  pages_categories_title = "pages_categories_title",
+}
+
+export default I18nKeys;
---
+++ src/locales/languages/jp.ts
@@ -0,0 +1,19 @@
+import type { Translation } from "../translation";
+import key from "../keys";
+
+export const jp: Translation = {
+  [key.copy_right_author]: "作者",
+  [key.copy_right_publish_date]: "公開日",
+  [key.copy_right_license]: "ライセンス",
+
+  [key.nav_bar_home]: "ホーム",
+  [key.nav_bar_about]: "概要",
+  [key.nav_bar_search_placeholder]: "検索",
+
+  [key.side_bar_categories]: "カテゴリー",
+  [key.side_bar_tags]: "タグ",
+  [key.side_bar_view_more]: "もっと見る",
+
+  [key.pages_tags_title]: "タグ",
+  [key.pages_categories_title]: "カテゴリー",
+};
