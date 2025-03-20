75412ffee42781316297502bbff7299f9e057a43
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2025-03-20 23:43:29 +0200

    Refactor Tailwind CSS integration and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://oniguy4649.github.io',
+  site: "https://oniguy4649.github.io",
+  output: 'static',
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+    },
+  },
 });
